1acaef0cfd108541ce732ffa4af6f02106c1c091
nainglinaung <nainglinaung91@gmail.com>
2023-11-06 11:59:47 +0630

    fix for token expire

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,31 +1,37 @@
 import './Global.css';
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useCookies } from 'react-cookie';
 import AuthProvider from '../providers/Auth';
-import TableLayout  from '../layouts/TableLayout'
+import TableLayout from '../layouts/TableLayout'
 import Navbar from '../layouts/navbar';
 import { TableData } from '../types/table';
 import SearchServiceClass from "../services/search.services";
+import { useNavigate } from 'react-router';
 
 
 function Home() {
 
-  const [cookie] = useCookies(["accessToken"]);
   const [data, setData] = useState<TableData[]>([]);
-   
-  
-    useEffect(() => {
-    
+  const navigate = useNavigate()
+  const [cookie, , removeCookie] = useCookies(['accessToken']);
+  useEffect(() => {
+
     const searchService = new SearchServiceClass(`Bearer ${cookie.accessToken}`);
     searchService.list().then((data) => {
-      setData(data);
+      setData(data)
+    }).catch((error) => {
+      if (error.message === 'HTTP status 401') {
+        removeCookie("accessToken", { path: '/' });
+        navigate('/login')
+      }
+
     })
-   
-  },[cookie])
+
+  }, [cookie])
 
 
   return (
-    <AuthProvider> 
+    <AuthProvider>
       <Navbar />
       <TableLayout table={data} />
     </AuthProvider>
--- src/services/fetch.service.ts
+++ src/services/fetch.service.ts
@@ -2,7 +2,7 @@ export default class FetchServiceClass {
 
     private headers;
     private baseURL;
-    
+
     constructor(header: object) {
         this.headers = new Headers();
         for (const [key, value] of Object.entries(header)) {
@@ -14,12 +14,16 @@ export default class FetchServiceClass {
     async post(url: string, payload: object): Promise<any> {
 
         const response = await fetch(`${this.baseURL}/${url}`, { method: 'POST', headers: this.headers, body: JSON.stringify(payload), redirect: 'follow' })
-        
+
         return response.json()
     }
 
-    async get(url:string): Promise<any> {
+    async get(url: string): Promise<any> {
+
         const response = await fetch(`${this.baseURL}/${url}`, { method: 'GET', headers: this.headers });
+        if (!response.ok) {
+            throw new Error("HTTP status " + response.status);
+        }
         return response.json()
     }
 
