74c3613c88d46f945195cd1bd46200545a84aeb7
nainglinaung <nainglinaung91@gmail.com>
2023-11-25 23:08:23 +0630

    layout changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+import Keywords from "./components/Keywords"
 import Navbar from "./components/Navbar"
 import Sidebar from "./components/Sidebar"
+
 export default function Home() {
   return (
     <main>
       <Navbar />
-      <Sidebar />
+      <div className="drawer lg:drawer-open">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col">
+          <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+          <Keywords />
+        </div> 
+        <Sidebar />
+      </div>
     </main>
   )
 }
